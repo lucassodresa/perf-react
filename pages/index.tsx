b11b5d83bd6843bcfb9f8cdf2eb065715837acd9
lucassodresa <lucas.sodresa@gmail.com>
2022-08-19 21:39:16 +0100

    project structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
 import type { NextPage } from "next";
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!search.trim()) return;
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+    setResults(data);
+  };
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <SearchResults results={results} />
     </div>
   );
 };
